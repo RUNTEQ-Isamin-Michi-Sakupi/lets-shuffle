3be01c73a7936d4726b9b305d6b703e5b4b08d89
mskk0803 <toyama2ura@gmail.com>
2025-01-14 16:41:43 +0900

    カード背面

---
+++ src/src/03_views/components/atoms/BackCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function BackCard({ name, index }) {
+    const cardStyle = {
+      width: "6rem",
+      aspectRatio: "58 / 89",
+      backgroundColor: "#c00606",
+      boxShadow: "0 0 6px black",
+      border: "0.5rem white solid",
+      borderRadius: "0.5rem" 
+    }
+  
+    return <div style={cardStyle} id={index} >{name}</div>;
+  }
+
+export default BackCard
