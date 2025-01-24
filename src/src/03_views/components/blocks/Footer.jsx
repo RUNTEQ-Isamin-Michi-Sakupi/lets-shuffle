88c03add607d9628daa63ba88506cf05f314a06c
michihiro721 <ms.michihiro.0721@gmail.com>
2025-01-24 21:51:24 +0900

    フッター動かないようにする

--- src/src/03_views/components/blocks/Footer.jsx
+++ src/src/03_views/components/blocks/Footer.jsx
@@ -7,11 +7,10 @@ const Footer = () => {
     backgroundSize: 'cover', // 画像をコンテナ全体にカバー
     backgroundPosition: 'center', // 画像を中央に配置
     backgroundRepeat: 'no-repeat', // 画像を繰り返さない
-    position: 'absolute', /*←絶対位置*/
+    position: 'fixed', /*←絶対位置*/
     bottom: 0, /*下に固定*/
     width: '100vw', // ビューポートの幅
     height: '5vh', // ビューポートの高さ
-    positon: 'fixed'
   }
 
   const Devex = {
